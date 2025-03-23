2c4ca4423110835e6d6d6095bbe1df437d001e95
P-Dubey294 <dpratham649@gmail.com>
2025-03-23 10:38:23 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData =  {
+  "login": "P-Dubey294",
+  "id": 174113079,
+  "node_id": "U_kgDOCmDBNw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/174113079?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/P-Dubey294",
+  "html_url": "https://github.com/P-Dubey294",
+  "followers_url": "https://api.github.com/users/P-Dubey294/followers",
+  "following_url": "https://api.github.com/users/P-Dubey294/following{/other_user}",
+  "gists_url": "https://api.github.com/users/P-Dubey294/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/P-Dubey294/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/P-Dubey294/subscriptions",
+  "organizations_url": "https://api.github.com/users/P-Dubey294/orgs",
+  "repos_url": "https://api.github.com/users/P-Dubey294/repos",
+  "events_url": "https://api.github.com/users/P-Dubey294/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/P-Dubey294/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "PrathamKumar Dubey",
+  "company": "Asterisc Technocrat Information Technology & Services",
+  "blog": "https://www.linkedin.com/in/pratham-ku-dubey-6b9440255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  "location": "Gondia Maharastra ",
+  "email": null,
+  "hireable": null,
+  "bio": "Full Stack Web Devloper🚀",
+  "twitter_username": null,
+  "public_repos": 2,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2024-06-28T11:49:49Z",
+  "updated_at": "2025-03-22T07:55:54Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello Pratham Dubey!')
 })
@@ -16,6 +52,10 @@ app.get('/login',(req,res) => {
 
 app.get('/youtube', (req,resp) => {
     resp.send('<h2> Eat sleep code repeate <h2>')
+})
+
+app.get('/github', (req,res) => {
+  res.json(githubData)
 })
 
 app.listen(port, () => {
